bd23abd052fe49b3d3c56a77e1b094aec5710047
theadg <deguzmanbernardandrew@gmail.com>
2022-11-28 18:43:56 +0800

    Add modules

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import DropDown from './components/dropDownMenu';
+import ImageSlider from './components/imageSlider';
+import './styles/style.scss';
+
+DropDown('#dropdown__header', '.dropdown__container');
+ImageSlider();
